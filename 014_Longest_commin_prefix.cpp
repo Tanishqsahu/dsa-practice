77d8df998250e98d4775498eb3ebebf335014af7
Tanishq Sahu <tanishqsahu7302@gmail.com>
2026-07-20 23:24:39 +0530

    Leetcode 14

---
+++ 014_Longest_commin_prefix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        
+        for(int i=0;i<strs[0].size();i++){
+            char c = strs[0][i];
+            for(int j=0;j<strs.size();j++){
+                if(i>=strs[j].size()){
+                    return ans;
+                    
+                }
+                if(strs[j][i]!=c){
+                    return ans;
+                }
+               
+
+            }
+            ans+=c;
+        }
+        return ans;
+
+
+
+
+        
+    }
+};
