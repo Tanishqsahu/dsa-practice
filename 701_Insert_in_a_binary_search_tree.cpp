c9fbe8be3b57c14e3b99260ffe3cbb38bea1b6e1
Tanishq Sahu <tanishqsahu7302@gmail.com>
2026-08-31 22:10:34 +0530

    Leetcode701 Appraoch2

--- 701_Insert_in_a_binary_search_tree.cpp
+++ 701_Insert_in_a_binary_search_tree.cpp
@@ -24,3 +24,36 @@ public:
         
     }
 };
+
+#Approach2
+
+class Solution{
+public:
+       TreeNode* insertIntoBST(TreeNode*root,int val){
+        if(root==NULL){
+            return new TreeNode(val);
+        }
+        TreeNode*curr =root;
+        while(true){
+            if(val<curr->val){
+                if(curr->left==NULL){
+                    curr->left=new TreeNode(val);
+                    break;
+                }
+                curr=curr->left;
+            }
+            if(val>curr->val){
+                if(curr->right==NULL){
+                    curr->right=new TreeNode(val);
+                    break;
+                }
+                curr=curr->right;
+            }
+
+        }
+
+
+        return root;
+       }
+
+};
