aadbee0afa70973af8ba8d4df8a37c4a638b0207
Tanishq Sahu <tanishqsahu7302@gmail.com>
2026-09-20 15:28:40 +0530

    Leetcode 733

---
+++ 0733_Flood_Fill.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+
+    void dfs(vector<vector<int>>& image,int r,int c,int original,int color){
+
+        if (r < 0 || r >= image.size() ||
+            c < 0 || c >= image[0].size()) {
+            return;
+        }
+
+        if(image[r][c]!=original){
+            return;
+        }
+        image[r][c]=color;
+
+        dfs(image, r - 1, c, original, color);
+
+        
+        dfs(image, r + 1, c, original, color);
+
+        
+        dfs(image, r, c - 1, original, color);
+
+        
+        dfs(image, r, c + 1, original, color);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
+        int original = image[sr][sc];
+
+        if(original==color){
+            return image;
+        }
+
+        dfs(image,sr,sc,original,color);
+
+        return image;
+
+
+        
+
+
+        
+        
+    }
+};
